adc369d5ee3a756b3ebfcc1fee72507ff313f0c2
Panji Rimawan <Panji Rimawan>
2016-07-03 00:04:28 +0700

    first upload

---
+++ contact.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="assets/img/favicon.png">
+	<title>Company Name | Contact Us</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="header clearfix">
+			<nav>
+				<ul class="nav nav-pills pull-right">
+					<li role="presentation"><a href="index.php">Home</a></li>
+					<li role="presentation"><a href="#">About</a></li>
+					<li role="presentation" class="active"><a href="contact.php">Contact</a></li>
+				</ul>
+				<h3 class="text-muted"><a href="index.php"><span><img src="assets/img/favicon.png"></span> Company</a></h3>
+			</nav>
+		</div>
+
+		<div class="row marketing">
+			<h1 class="text-center title-page">CONTACT US</h1>
+			<div class="col-lg-6">
+				<h3><i class="fa fa-home"></i> More Information</h3>
+				<ul class="fa-ul">
+					<li>
+						<i class="fa-li fa fa-map-marker"></i><span>Jalan Teknik Hidrodinamika VIII 98, Surabaya, Jawa Timur, Indonesia</span>
+					</li>
+					<li>
+						<i class="fa-li fa fa-envelope"></i><span>info@company.org</span>
+					</li>
+					<li>
+						<i class="fa-li fa fa-phone"></i><span>+6231 1234567 (<i>Nakula</i>) <br>+6281 2345 6789 (<i>Sadewa</i>)</span>
+					</li>
+				</ul>
+			</div>
+			<div class="col-lg-6">
+				<h3><i class="fa fa-search"></i> Discover Our Social Media</h3>
+				<ul class="fa-ul">
+					<li>
+						<i class="fa-li fa fa-facebook"></i><span>/Company Org</span>
+					</li>
+					<li>
+						<i class="fa-li fa fa-twitter"></i><span>@company_org</span>
+					</li>
+					<li>
+						<i class="fa-li fa fa-instagram"></i><span>@company_org</span>
+					</li>
+					<li>
+						<i class="fa-li fa fa-youtube-play"></i><span>/Company_Org</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="jumbotron">
+			<h4 style="margin-top: -15px">See our company profile's video</h4>
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/9No-FiEInLA" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>
+
+		<footer class="footer">
+			<p>Copyright &copy; Company Name 2016</p>
+		</footer>
+	</div>
+
+	<script type="text/javascript" src="assets/js/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
