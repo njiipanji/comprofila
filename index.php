3db21832ef7b8bc6f8f8123421f66be221a94cbf
Panji Rimawan <Panji Rimawan>
2016-07-04 05:25:00 +0700

    delete latest news

--- index.php
+++ index.php
@@ -43,25 +43,6 @@
 			<a class="right carousel-control" href="#slideshow" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>
 		</div>
 
-		<hr class="featurette-divider">
-
-		<div class="row marketing">
-			<div class="col-md-7 col-lg-7">
-				<h2 class="featurette-heading"><i class="fa fa-list-alt" aria-hidden="true"></i> Latest News</h2>
-				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-			<div class="col-md-5 col-lg-5">
-				<img src="assets/img/news.png" style="width:100%;">
-			</div>
-		</div>
-
-		<hr class="featurette-divider">
-
 		<div class="row marketing">
 			<div class="col-lg-4">
 				<h3><i class="fa fa-group"></i> IBW Group</h3>
