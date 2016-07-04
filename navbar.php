955aee9ee4aa46adce7c87d6d74ea4f7956963f7
Panji Rimawan <Panji Rimawan>
2016-07-04 09:36:51 +0700

    add navbar-menu

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<div class="row marketing" style="padding: 0px 15px">
+	<ul class="nav nav-pills nav-justified" id="bigmenu">
+		<li role="presentation" class="dropdown">
+			<a href="#" data-toggle="dropdown" class="dropdown-toggle menu-nav"><i class="fa fa-group" aria-hidden="true"></i> IBW Group <b class="caret"></b></a>
+			<ul class="dropdown-menu" id="ibw-menu">
+				<li id="ibw-content">
+					<a href="#"><i class="fa fa-user" aria-hidden="true"></i> Founder</a>
+					<a href="#"><i class="fa fa-cogs" aria-hidden="true"></i> Management</a>
+					<a href="#"><i class="fa fa-bank" aria-hidden="true"></i> Corporate Value</a>
+				</li>
+			</ul>
+		</li>
+		<li role="presentation" class="dropdown">
+			<a href="#" data-toggle="dropdown" class="dropdown-toggle menu-nav"><i class="fa fa-building" aria-hidden="true"></i> Bussiness Unit <b class="caret"></b></a>
+		</li>
+		<li role="presentation"><a href="contact.php" class="menu-nav"><i class="fa fa-envelope" aria-hidden="true"></i> Contact Us</a></li>
+	</ul>
+</div>
