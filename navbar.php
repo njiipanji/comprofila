76dc545e87fe51faa0ae175b4a15ccd58b4cae0b
Panji Rimawan <Panji Rimawan>
2016-07-05 14:28:20 +0700

    update navbar ibw, unit

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
 		<li role="presentation" class="dropdown">
 			<a href="#" data-toggle="dropdown" class="dropdown-toggle menu-nav"><i class="fa fa-group" aria-hidden="true"></i> IBW Group <b class="caret"></b></a>
 			<ul class="dropdown-menu" id="ibw-menu">
-				<li id="ibw-content">
+				<li>
 					<a href="#"><i class="fa fa-user" aria-hidden="true"></i> Founder</a>
 					<a href="#"><i class="fa fa-cogs" aria-hidden="true"></i> Management</a>
 					<a href="#"><i class="fa fa-bank" aria-hidden="true"></i> Corporate Value</a>
@@ -12,6 +12,60 @@
 		</li>
 		<li role="presentation" class="dropdown">
 			<a href="#" data-toggle="dropdown" class="dropdown-toggle menu-nav"><i class="fa fa-building" aria-hidden="true"></i> Bussiness Unit <b class="caret"></b></a>
+			<ul class="dropdown-menu" id="unit-menu">
+				<li class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="row">
+						<div class="col-md-3 col-lg-3 unit-content">
+							<a href="#">
+								<img src="assets/img/unit.png" class="img-responsive">
+								<span class="img-unit-content">UNIT 1</span>
+							</a>
+						</div>
+						<div class="col-md-3 col-lg-3 unit-content">
+							<a href="#">
+								<img src="assets/img/unit.png" class="img-responsive">
+								<span class="img-unit-content">UNIT 2</span>
+							</a>
+						</div>
+						<div class="col-md-3 col-lg-3 unit-content">
+							<a href="#">
+								<img src="assets/img/unit.png" class="img-responsive">
+								<span class="img-unit-content">UNIT 3</span>
+							</a>
+						</div>
+						<div class="col-md-3 col-lg-3 unit-content">
+							<a href="#">
+								<img src="assets/img/unit.png" class="img-responsive">
+								<span class="img-unit-content">UNIT 4</span>
+							</a>
+						</div>
+					</div>
+<!--					<div class="unit-content">
+						<a href="#">
+							<img src="assets/img/unit.png">
+							<span class="img-unit-content">Unit 1</span>
+						</a>
+					</div>
+					<div class="unit-content">
+						<a href="#">
+							<img src="assets/img/unit.png">
+							<span class="img-unit-content">Unit 2</span>
+						</a>
+					</div>
+					<div class="unit-content">
+						<a href="#">
+							<img src="assets/img/unit.png">
+							<span class="img-unit-content">Unit 3</span>
+						</a>
+					</div>
+					<div class="unit-content">
+						<a href="#">
+							<img src="assets/img/unit.png">
+							<span class="img-unit-content">Unit 4</span>
+						</a>
+					</div> -->
+				</li>
+			</ul>
 		</li>
 		<li role="presentation"><a href="contact.php" class="menu-nav"><i class="fa fa-envelope" aria-hidden="true"></i> Contact Us</a></li>
 	</ul>
