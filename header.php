11fc9f458a2aad750c36ba987919af70a428a58c
Panji Rimawan <Panji Rimawan>
2016-08-15 12:31:52 +0700

    add header template

---
+++ header.php
@@ -0,0 +1,22 @@
+<div class="header clearfix">
+	<nav>
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navmobile">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<h3 class="text-muted" style="padding-top: 5px"><a href="index.php"><span><img src="assets/img/favicon.png"></span> Company</a></h3>
+			</div>
+			<div class="collapse navbar-collapse" id="navmobile">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</div>
